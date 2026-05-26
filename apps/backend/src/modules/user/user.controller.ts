b0f551a28129dfad243d1613920c19a4b1b00350
Ashot08 <albertguzeev@yandex.ru>
2026-05-26 22:21:13 +0700

    add validation pipe

--- apps/backend/src/modules/user/user.controller.ts
+++ apps/backend/src/modules/user/user.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from '@/modules/user/dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create (@Body() createUserDto: CreateUserDto) {
+    return await this.userService.create(createUserDto);
+  }
+
 }
--- apps/backend/src/modules/user/user.service.ts
+++ apps/backend/src/modules/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '@/modules/user/dto/create-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '@/modules/user/entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
+  async create(userDto: CreateUserDto) {
+    await this.userRepository.save(userDto);
+  }
+}
