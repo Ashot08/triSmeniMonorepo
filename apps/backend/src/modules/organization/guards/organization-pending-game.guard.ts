7d9153d97822fac907d7813aed8559afb07bb8e0
Ashot08 <albertguzeev@yandex.ru>
2026-08-20 12:31:15 +0700

    fix: remove adding PendingGame to req in guards

--- apps/backend/src/modules/organization/guards/organization-pending-game.guard.ts
+++ apps/backend/src/modules/organization/guards/organization-pending-game.guard.ts
@@ -1,7 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { PendingGameRepository } from '@/modules/game/pending-game/pending-game.repository';
 import { PendingGameNotFoundError } from '@/modules/game/pending-game/errors/pending-game-not-found.error';
-import { PendingGame } from '@/modules/game/pending-game/pending-game';
 import { Request } from 'express';
 
 interface OrganizationPendingGameRequest extends Request {
@@ -9,7 +8,6 @@ interface OrganizationPendingGameRequest extends Request {
     organizationId: string;
     gameId: string;
   };
-  game: PendingGame | null;
 }
 
 @Injectable()
@@ -29,8 +27,6 @@ export class OrganizationPendingGameGuard implements CanActivate {
     if (!game) {
       throw new PendingGameNotFoundError();
     }
-
-    req.game = game;
 
     return true;
   }
